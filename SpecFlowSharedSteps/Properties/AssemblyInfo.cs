ce63df298b4307e85b6d563559183bb6e014a7f5
Bakanych <manych@gmail.com>
2017-07-23 18:28:19 +0800

    title

--- SpecFlowSharedSteps/Properties/AssemblyInfo.cs
+++ SpecFlowSharedSteps/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SpecFlow.SharedSteps")]
+[assembly: AssemblyTitle("SpecFlow Shared Steps")]
 [assembly: AssemblyDescription("This package contains SpecFlow steps definitions which allow you to reuse scenario steps")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Alexander Bakanov")]
-[assembly: AssemblyProduct("SpecFlow.SharedSteps")]
+[assembly: AssemblyProduct("SpecFlow Shared Steps")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
